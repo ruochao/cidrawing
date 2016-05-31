d30501599344b03d270a25e9246263fff93ca29a
ruochao <ruochao@gmail.com>
2016-06-01 00:18:16 +0800

    Fine tune smooth stroke mode;

--- cidrawing/src/main/java/com/mocircle/cidrawing/mode/stroke/SmoothStrokeMode.java
+++ cidrawing/src/main/java/com/mocircle/cidrawing/mode/stroke/SmoothStrokeMode.java
@@ -28,6 +28,9 @@ public class SmoothStrokeMode extends BaseStrokeMode {
     protected CiPaint assignPaint() {
         CiPaint paint = new CiPaint(drawingContext.getPaint());
         paint.setStyle(Paint.Style.STROKE);
+        paint.setAntiAlias(true);
+        paint.setStrokeJoin(Paint.Join.ROUND);
+        paint.setStrokeCap(Paint.Cap.ROUND);
         if (smoothRadius > 0) {
             CornerPathEffect effect = new CornerPathEffect(smoothRadius);
             if (paint.getPathEffect() == null) {
